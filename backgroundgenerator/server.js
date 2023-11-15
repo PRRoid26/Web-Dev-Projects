3d294dea858779615821fb54d43405597fda7dfe
PRRoid26 <easwaranshetty@gmail.com>
2023-11-15 15:57:40 +0530

    3 projects

---
+++ backgroundgenerator/server.js
@@ -0,0 +1,37 @@
+const http = require('http');
+const fs = require('fs');
+const path = require('path');
+
+const server = http.createServer((req, res) => {
+    const filePath = path.join(__dirname, req.url === '/' ? 'login.html' : req.url.substring(1));
+    const contentType = getContentType(filePath);
+
+    fs.readFile(filePath, (err, data) => {
+        if (err) {
+            res.writeHead(404, { 'Content-Type': 'text/html' });
+            res.end('404 Not Found');
+        } else {
+            res.writeHead(200, { 'Content-Type': contentType });
+            res.end(data);
+        }
+    });
+});
+
+const port = process.env.PORT || 8080;
+server.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
+});
+
+function getContentType(filePath) {
+    const extname = path.extname(filePath);
+    switch (extname) {
+        case '.html':
+            return 'text/html';
+        case '.css':
+            return 'text/css';
+        case '.js':
+            return 'text/javascript';
+        default:
+            return 'text/plain';
+    }
+}
